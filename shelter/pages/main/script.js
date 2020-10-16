cf31af003b7e561388a828943619c73b02f39dbd
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2020-10-16 20:14:16 +0300

    feat: add popup

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,43 +1,75 @@
-// const navigationItem = document.querySelectorAll('.navigation-link'),
-//     heroButton = document.querySelector('.hero-button'),
-//     petsButton = document.querySelector('.pets-button');
-
-
-// toThePets()
-// disableInactive()
-
 let shelterObject = {
-    classes: {
-        navigationItem: document.querySelectorAll('.navigation-link'),
-        heroButton: document.querySelector('.hero-button'),
-        petsButton: document.querySelector('.pets-button')
-    },
-    
-    pets: [],
-    toThePets() {
-        this.classes.heroButton.addEventListener('click', function(){
-            document.location='../pets/index.html'
-        })
-        this.classes.petsButton.addEventListener('click', function(){
-            document.location='../pets/index.html'
-        })
-        console.log(this.pets)
-    },
-    disableInactive() {
-        for (let i = 2; i<this.classes.navigationItem.length; i++) {
-            this.classes.navigationItem[i].style.pointerEvents = 'none';
-        }
-    },
-    loadPets() {
-        let response = fetch('../main/data.json')
-       let arrayPets =  response.json()
-       this.pets = arrayPets.pets;
-    },
-    init() {
-        this.loadPets()
-        this.disableInactive()
-        this.toThePets()
-    }
+        classes: {
+            navigationItem: document.querySelectorAll('.navigation-link'),
+            heroButton: document.querySelector('.hero-button'),
+            petsButton: document.querySelector('.pets-button'),
+            petsSlider: document.querySelector('.pets__slider'),
+            modalWindow: document.querySelector('#modal'),
+            overlay: document.querySelector('#overlay'),
+            modalName: document.querySelector('.modal-name'),
+            typeBreed: document.querySelector('.type-breed'),
+            modalmodalDescription: document.querySelector('.modal__description'),
+            petAge: document.querySelector('.age-info'),
+            petInoculations: document.querySelector('.inoculations-info'),
+            petDiseases: document.querySelector('.diseases-info'),
+            petParasites: document.querySelector('.parasites-info'),
+            modalImg: document.querySelector('.modal-img')
+        },
 
-}
-shelterObject.init()
+        pets: [],
+        toThePets() {
+            this.classes.heroButton.addEventListener('click', function () {
+                document.location = '../pets/index.html'
+            })
+            this.classes.petsButton.addEventListener('click', function () {
+                document.location = '../pets/index.html'
+            })
+        },
+        disableInactive() {
+            for (let i = 2; i < this.classes.navigationItem.length; i++) {
+                this.classes.navigationItem[i].style.pointerEvents = 'none';
+            }
+        },
+        sendRequest() {
+            return fetch('../main/data.json').then(response => response.json()).then(petsData => this.configurePets(petsData))
+        },
+        configurePets(petsData) {
+            this.pets = petsData;
+            let popupItem;
+            this.classes.petsSlider.addEventListener('click', function (e) {
+                let petsBlock = e.target.closest('.pets__slider__item');
+                let popupName = petsBlock.childNodes[3].innerText
+                petsData.map((item) => {
+                    if (item.name == popupName) {
+                        popupItem = item;
+                    }
+                })
+                shelterObject.classes.modalName.textContent = popupItem.name;
+                shelterObject.classes.typeBreed.textContent = `${popupItem.type} - ${popupItem.breed}`;
+                shelterObject.classes.modalmodalDescription.textContent = popupItem.description;
+                shelterObject.classes.petAge.textContent = popupItem.age;
+                shelterObject.classes.petInoculations.textContent = popupItem.inoculations;
+                shelterObject.classes.petDiseases.textContent = popupItem.diseases;
+                shelterObject.classes.petParasites.textContent = popupItem.parasites;
+                shelterObject.classes.modalImg.src = popupItem.img
+                if (petsBlock) {
+                    shelterObject.classes.modalWindow.classList.add('active')
+                    shelterObject.classes.overlay.classList.add('active')
+                }
+            })
+            this.classes.overlay.addEventListener('click', function (e) {
+                    if (shelterObject.classes.overlay.classList.contains('active')) {
+                        shelterObject.classes.overlay.classList.remove('active')
+                        shelterObject.classes.modalWindow.classList.remove('active')
+                    }
+                })
+                },
+
+                init() {
+                    this.sendRequest()
+                    this.disableInactive()
+                    this.toThePets()
+                }
+
+            }
+            shelterObject.init()
